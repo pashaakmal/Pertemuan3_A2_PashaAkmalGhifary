1fe6ffa291472e5b051e8c6bf4fdc7bf3cd46a0a
pashaakmal <143366356+pashaakmal@users.noreply.github.com>
2023-08-28 09:58:35 +0700

    Add files via upload

---
+++ soal.py
@@ -0,0 +1,5 @@
+feeling = input("how are you?")
+if feeling.lower() == 'great':
+    print('jos tenan')
+else:
+    print('wkwkkwkkwkwk')
---
+++ soal2.py
@@ -0,0 +1,9 @@
+#soal 2
+m = ['Alice', 'ants', 'Bob', 'badgers', 'Carol', 'cats'] 
+m.sort()
+m
+print(m)
+spam = ['a', 'z', 'A', 'Z'] 
+spam.sort(key=str.lower) 
+spam
+print(spam)
